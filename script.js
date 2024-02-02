0ed0d266767afc30739066b3d3dd91876897c710
FabianCiceia <ciceiafabian@gmail.com>
2024-02-02 00:54:38 -0300

    agregando consiccionales a la calculadora

---
+++ script.js
@@ -0,0 +1,41 @@
+var display = document.getElementById("display");
+var valor = "";
+var numero = true; //evita que se pongan dos operadores al mismo tiempo ejemplo ++ -- // **
+var cal = false;  //evita el escribir numeros al resultado de una operacion
+function press(tecla){
+    if(!cal){
+        valor+=tecla;
+        display.innerHTML = valor;
+        numero = true;
+    }else{
+        valor=0;
+        valor+=tecla;
+        display.innerHTML = valor;
+        numero = true;
+        cal = false;
+    }
+}
+function setOP(operador){
+    if(numero){
+        valor+=operador;
+        display.innerHTML = valor;
+        numero = false;
+        cal = false;
+    }
+}
+function clr(){
+    valor = "";
+    display.innerHTML = "0";
+}
+function calculate(){
+    if(numero && !(valor == undefined) && !(valor == 0)){
+        if(eval(valor) % 1){
+            valor = String((eval(valor)).toFixed(2));
+            display.innerHTML = valor;
+        }else{
+            valor = String(eval(valor));
+            display.innerHTML = valor;
+        }
+        cal = true;
+    }
+}
