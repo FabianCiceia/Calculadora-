9ce7f1170c0f1f965721af031e0b964261a4a77a
FabianCiceia <ciceiafabian@gmail.com>
2024-02-02 01:35:31 -0300

    corrigiendo bug de poder colocar punto a un numero que ya lo contenia

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var display = document.getElementById("display");
 var valor = "";
 var numero = true; //evita que se pongan dos operadores al mismo tiempo ejemplo ++ -- // **
 var cal = false;  //evita el escribir numeros al resultado de una operacion
+var punto = true; //para evitar que se coloquen doble punto ejemplo: 5.2.8
 function press(tecla){
     if(!cal){
         valor+=tecla;
@@ -13,6 +14,7 @@ function press(tecla){
         display.innerHTML = valor;
         numero = true;
         cal = false;
+        punto = true;
     }
     
 
@@ -24,20 +26,37 @@ function setOP(operador){
         display.innerHTML = valor;
         numero = false;
         cal = false;
+        punto = true;
     }
 }
+function Punt(PUN){
+    if(punto){
+        valor+=PUN;
+        display.innerHTML = valor;
+        numero = false;
+        cal = false;
+        punto = false;
+    }
+}
+
 function clr(){
     valor = "";
     display.innerHTML = "0";
+    numero = false;
+    punto = true;
 }
+
+
 function calculate(){
     if(numero && !(valor == undefined) && !(valor == 0)){
         if(eval(valor) % 1){
             valor = String((eval(valor)).toFixed(2));
             display.innerHTML = valor;
+            punto = false;
         }else{
             valor = String(eval(valor));
             display.innerHTML = valor;
+            punto = true;
         }
         cal = true;
     }
